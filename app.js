438c81e64dd10d1b7535607406551f46f800f40f
mt2k24 <bigbrother2k23@gmail.com>
2024-02-08 20:50:26 +0530

    The  Game Is ready

--- app.js
+++ app.js
@@ -44,15 +44,15 @@ boxes.forEach((box) => {
 });
 
 const disableBoxes = () => {
-    for(let box of boxes) {
+    for (let box of boxes) {
         box.disabled = true;
     }
 }
 
 const enableBoxes = () => {
-    for(let box of boxes) {
+    for (let box of boxes) {
         box.disabled = false;
-        box.innerText ="";
+        box.innerText = "";
     }
 }
 
@@ -60,27 +60,45 @@ const showWinner = (winner) => {
     msg.innerText = `Congratulation, winner is ${winner}`;
     msgContainer.classList.remove("hide");
     disableBoxes();
+    setTimeout(resetGame, 10000); // Reset the game after 10 seconds
+};
+
+const checkDraw = () => {
+    // Check if all boxes are filled
+    return boxes.every(box => box.innerText !== "");
 };
 
 const checkWinner = () => {
+    let winnerFound = false;
     for (let pattern of winPatterns) {
         let pos1Val = boxes[pattern[0]].innerText;
         let pos2Val = boxes[pattern[1]].innerText;
         let pos3Val = boxes[pattern[2]].innerText;
 
-        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+        if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
             if (pos1Val === pos2Val && pos2Val === pos3Val) {
                 // There's a winner
                 console.log("Winner:", pos1Val);
                 // You can display the winner message or perform other actions here
-                showWinner(pos1Val)
+                showWinner(pos1Val);
+                winnerFound = true;
+                break; // Exit the loop if there's a winner
             }
         }
+    }
+
+    // If no winner is found, check for a draw
+    if (!winnerFound && checkDraw()) {
+        console.log("It's a draw!");
+        msg.innerText = "It's a draw!";
+        msgContainer.classList.remove("hide");
+        disableBoxes();
+        setTimeout(resetGame, 10000); // Reset the game after 10 seconds
     }
 };
 
 // Event listener for reset button
-resetbtn.addEventListener("click" , resetGame);
+resetbtn.addEventListener("click", resetGame);
 
 // Event listener for new game button
 newGameBtn.addEventListener("click", resetGame);
